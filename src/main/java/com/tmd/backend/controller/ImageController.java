1d828fda66fae6b2b91c980a12563f371cf695b9
eengeop <leeeus99@gmail.com>
2026-08-11 02:54:19 +0900

    chore: ImageResponseDTO 디렉터리 이름 변경

--- src/main/java/com/tmd/backend/controller/ImageController.java
+++ src/main/java/com/tmd/backend/controller/ImageController.java
@@ -1,7 +1,7 @@
 package com.tmd.backend.controller;
 
 import com.tmd.backend.dto.response.SuccessResponseDto;
-import com.tmd.backend.dto.response.file.FileResponse;
+import com.tmd.backend.dto.response.image.FileResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
