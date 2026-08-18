5e3faca19551bf44dac11856e51c55aaa2d9a801
nicealswls <nicealswls@naver.com>
2026-08-19 02:30:59 +0900

    wip: 마이페이지 GET /users/me 작업 중

--- src/main/java/com/tmd/backend/repository/UserRepository.java
+++ src/main/java/com/tmd/backend/repository/UserRepository.java
@@ -12,4 +12,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmailAndProvider(String email, AuthProvider provider);
 
     Optional<User> findByProviderAndProviderId(AuthProvider provider, String providerId);
+
+    // email 컬럼으로 찾기 명령 -> spring이 인식해서 SQL 생성해 줌
+    // Optional<User>: User가 있을 수도 없을 수도 있다 -> NullPointerException 방지
+    Optional<User> findByEmail(String email);
 }
