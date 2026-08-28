cfd88ac79985a929f1d0bb598aa1bbd892e11556
eengeop <leeeus99@gmail.com>
2026-08-28 23:36:46 +0900

    feat: Keyword 검색 Redis Cache 기능 추가

--- src/main/java/com/tmd/backend/service/KeywordCacheService.java
+++ src/main/java/com/tmd/backend/service/KeywordCacheService.java
@@ -1,4 +1,23 @@
 package com.tmd.backend.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Service
+@RequiredArgsConstructor
 public class KeywordCacheService {
+    private static final String KEY_PREFIX_FOR_KEYWORD_SEARCHING = "keyword-fetched";
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public boolean isFetched(String keyword){
+        return Boolean.TRUE.equals(redisTemplate.hasKey(KEY_PREFIX_FOR_KEYWORD_SEARCHING + keyword));
+    }
+
+    public void markFetched(String keyword){
+        redisTemplate.opsForValue().set(KEY_PREFIX_FOR_KEYWORD_SEARCHING, "true", Duration.ofDays(14));
+    }
 }
