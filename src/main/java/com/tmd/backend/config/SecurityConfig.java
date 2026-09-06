d137e50f25a16f4baaa3e69a7fc7b99a7dd35808
웅 <leeeus99@gmail.com>
2026-09-06 18:15:39 +0900

    refactor: 개발단계 권한 해제

--- src/main/java/com/tmd/backend/config/SecurityConfig.java
+++ src/main/java/com/tmd/backend/config/SecurityConfig.java
@@ -54,11 +54,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfiguration(){
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173")); // 프론트엔드 개발 포트에 맞춰야함.
+        corsConfiguration.setAllowedOrigins(List.of("*")); // 프론트엔드 개발 포트에 맞춰야함.
         corsConfiguration.setAllowedHeaders(List.of("*"));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setExposedHeaders(List.of("Authorization", "Set-Cookie"));
+        corsConfiguration.setExposedHeaders(List.of("Authorization"));
         UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
         urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
         return urlBasedCorsConfigurationSource;
