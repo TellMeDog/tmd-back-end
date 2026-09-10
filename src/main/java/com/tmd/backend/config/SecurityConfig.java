7e93c57bdc3a4ad4a225613f2c57b1563ab17710
eengeop <leeeus99@gmail.com>
2026-09-11 02:24:26 +0900

    chore: CORS 및 Redis 설정 정리

--- src/main/java/com/tmd/backend/config/SecurityConfig.java
+++ src/main/java/com/tmd/backend/config/SecurityConfig.java
@@ -59,9 +59,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfiguration(){
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000")); // 프론트엔드 개발 포트에 맞춰야함.
+        corsConfiguration.setAllowedOrigins(List.of("https://tellmedog.vercel.app", "http://localhost:5173")); // 프론트엔드 개발 포트에 맞춰야함.
         corsConfiguration.setAllowedHeaders(List.of("*"));
-        corsConfiguration.setAllowedMethods(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        corsConfiguration.setAllowCredentials(true); // 쿠키 수신을 위한 설정
+        corsConfiguration.setExposedHeaders(List.of("Authorization")); // 응답 헤더에 AccessToken을 담기 위한 설정
         UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
         urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
         return urlBasedCorsConfigurationSource;
