93c9eb5bf8cda977bcdf919eb93c5c5c07f1bf86
eengeop <leeeus99@gmail.com>
2026-09-13 01:29:08 +0900

    refactor: Pet 도메인 타입, 필드명 수정)

--- src/main/java/com/tmd/backend/domain/pet/Pet.java
+++ src/main/java/com/tmd/backend/domain/pet/Pet.java
@@ -11,7 +11,8 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Pet {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
@@ -19,39 +20,53 @@ public class Pet {
     @Enumerated(value = EnumType.STRING)
     private PetBreed breed;
 
-    @Enumerated(value = EnumType.STRING)
-    private PetSize size;
+    private Integer weight;
 
-    @Column(name = "image_url")
+    @Column(name = "image_key")
     private String imageKey;
 
+    private boolean hasMuzzle;      // 입마개 유무 추가
+    private boolean hasLeash;        // 목줄 유무 추가
+    private boolean hasCarrier;       // 이동장 유무 추가
+
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="user_id")
+    @JoinColumn(name = "user_id")
     private User user;
 
     @Builder
-    public Pet(User user, String name, PetBreed breed, PetSize size, String imageKey) {
-        this.user = user;
+    public Pet(User user, String name, PetBreed breed, Integer weight, String imageKey, boolean hasMuzzle, boolean hasLeash, boolean hasCarrier) {
         this.name = name;
         this.breed = breed;
-        this.size = size;
+        this.weight = weight;
         this.imageKey = imageKey;
+        this.hasMuzzle = hasMuzzle;
+        this.hasLeash = hasLeash;
+        this.hasCarrier = hasCarrier;
+        this.user = user;
     }
 
-    public static Pet create(User user, String name, PetBreed breed, PetSize size, String imageKey) {
+
+    public static Pet create(User user, String name, PetBreed breed, Integer weight, String imageKey, boolean hasMuzzle, boolean hasLeash, boolean hasCarrier) {
         return Pet.builder()
             .user(user)
             .name(name)
             .breed(breed)
-            .size(size)
+            .weight(weight)
             .imageKey(imageKey)
+            .hasMuzzle(hasMuzzle)
+            .hasLeash(hasLeash)
+            .hasCarrier(hasCarrier)
             .build();
     }
 
-    public void update(String name, PetBreed breed, PetSize size, String imageKey) {
+    public void update(String name, PetBreed breed, Integer weight, String imageKey,
+                       Boolean hasMuzzle, Boolean hasLeash, Boolean hasCarrier) {
         if (name != null) this.name = name;
         if (breed != null) this.breed = breed;
-        if (size != null) this.size = size;
-        this.imageKey = imageKey;
+        if (weight != null) this.weight = weight;
+        if (imageKey != null) this.imageKey = imageKey;
+        if (hasMuzzle != null) this.hasMuzzle = hasMuzzle;
+        if (hasLeash != null) this.hasLeash = hasLeash;
+        if (hasCarrier != null) this.hasCarrier = hasCarrier;
     }
 }
