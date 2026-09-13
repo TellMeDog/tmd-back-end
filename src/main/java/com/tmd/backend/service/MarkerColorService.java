fbd7cb97eff73a793436de5e268bb4b8dc932860
eengeop <leeeus99@gmail.com>
2026-09-13 16:52:40 +0900

    feat: 기본 정책에 따른 마커 색상 판정 추가

--- src/main/java/com/tmd/backend/service/MarkerColorService.java
+++ src/main/java/com/tmd/backend/service/MarkerColorService.java
@@ -1,17 +1,25 @@
 package com.tmd.backend.service;
 
 import com.tmd.backend.domain.pet.Pet;
+import com.tmd.backend.domain.pet.PetBreed;
 import com.tmd.backend.domain.place.PlacePetPolicy;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service
 public class MarkerColorService {
 
+    private static final Set<PetBreed> dangerousBreed = Set.of(PetBreed.DALMATIAN);
+
     public String calculateMarkerColor(PlacePetPolicy policy, Pet pet) {
         //TODO: 마커 색깔 판단 로직 구현
-        /*if(info.getStatus().equals(PetInfoStatus.NO_DATA)){
+        if (policy == null) {
             return "GREY";
-        }*/
+        }
+        if ("YELLOW".equals(policy.getDefaultPolicy())) {
+            return "YELLOW";
+        }
         return "GREEN";
     }
 }
