c183a748f0318cbaa57ee45733210fec717fc061
eengeop <leeeus99@gmail.com>
2026-09-13 17:18:39 +0900

    chore: 개발 환경 API 접근 및 R2 설정 변경

--- src/main/java/com/tmd/backend/config/SecurityConfig.java
+++ src/main/java/com/tmd/backend/config/SecurityConfig.java
@@ -35,12 +35,7 @@ public class SecurityConfig {
         http
             .authorizeHttpRequests(auth ->
                 auth
-                    .requestMatchers("/auth/login",
-                        "/auth/signup", "/auth/send-verification-code", "/auth/verify-email","/auth/reissue",
-                        "/swagger-ui/**", "/v3/api-docs/**",
-                        "/oauth2/**", "/login/oauth2/**",
-                        "/admin/**").permitAll()
-                .anyRequest().authenticated())
+                    .anyRequest().permitAll())
             .csrf(AbstractHttpConfigurer::disable)
             .httpBasic(AbstractHttpConfigurer::disable)
             .formLogin(AbstractHttpConfigurer::disable)
